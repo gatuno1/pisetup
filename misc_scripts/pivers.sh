ec82aa9341d69a6575909b154d0c358b34958fc9
Antonio Sanhueza <antonio_sanhueza@hotmail.com>
2016-07-14 22:57:11 -0400

    Updade descriptions and more models
    Get descriptions from RPi Hardware History elinux.org (http://elinux.org/RPi_HardwareHistory) to select more RPi models.

--- misc_scripts/pivers.sh
+++ misc_scripts/pivers.sh
@@ -2,27 +2,40 @@
 CPU=`cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | sed -e "s/ //g"`
 HW=`cat /proc/cpuinfo | grep Hardware | cut -d ':' -f 2 | sed -e "s/ //g"`
 REV=`cat /proc/cpuinfo | grep Revision | cut -d ':' -f 2 | sed -e "s/ //g"`
+#values from http://elinux.org/RPi_HardwareHistory
 case ${REV} in
-  a22082)
-    echo 'Pi 3, Model B'
+  a02082|a22082)
+    echo 'Pi 3 Model B, 1024MB RAM'
     ;;
-  a02082)
-    echo 'Pi 3, Model B'
+  a01040|a01041|a21041)
+    echo 'Pi 2 Model B, 1024MB RAM'
     ;;
-  a21041)
-    echo 'Pi 2, Model B'
+  900092|900093)
+    echo 'Pi Zero, 512MB RAM'
     ;;
-  a01041)
-    echo 'Pi 2, Model B'
+  0012|0015)
+    echo 'Pi Model A+, 256MB RAM'
     ;;
-  900092)
-    echo 'Pi Zero'
+  0010|0013)
+    echo 'Pi, Model B+, 512MB RAM'
     ;;
-  0012)
-    echo 'Pi, Model A+'
+  0011|0014)
+    echo 'Compute Module, 512MB RAM'
     ;;
-  0010)
-    echo 'Pi, Model B+'
+  0002|0004|0005|0006)
+    echo 'Pi Model B, 256MB RAM'
+    ;;
+  000d|000e|000f)
+    echo 'Pi Model B, 512MB RAM'
+    ;;
+  0003)
+    echo 'Pi Model B (ECN0001), 256MB RAM'
+    ;;
+  Beta)
+    echo 'Pi, Model B (Beta), 256MB RAM'
+    ;;
+  0007|0008|0009)
+    echo 'Pi, Model A, 256MB RAM'
     ;;
   *)
     echo '${REV} (unknown)'
